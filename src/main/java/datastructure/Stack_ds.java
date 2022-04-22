a2ad31a31c0f1923a219364d3a3a961a8f8d0c59
Gokula Kannan L <96760784+gokulakannan-tringapps@users.noreply.github.com>
2022-04-22 11:52:23 +0530

    Update Stack_ds.java

--- src/main/java/datastructure/Stack_ds.java
+++ src/main/java/datastructure/Stack_ds.java
@@ -19,7 +19,7 @@ public class Stack_ds {
 	
 	
 	
-		public static void main(String args[]) {
+		public static void main(String[] args) {
 			
 			Stack<Integer> stack=new Stack<Integer>();
 			int choice;
